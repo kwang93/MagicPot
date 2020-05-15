9a11d4b372f258d0e0b9372845bcc82358f0708a
Kenny Wang <kwang@oberlin.edu>
2020-05-15 00:38:53 -0400

    final edits

--- guiMain.py
+++ guiMain.py
@@ -4,13 +4,10 @@ if sys_pf == 'darwin':
     import matplotlib
     matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, FigureCanvasAgg
 import PySimpleGUI as sg
-import matplotlib.backends.tkagg as tkagg
-from matplotlib.figure import Figure
 import serial
 import time
-import tkinter as Tk
+import os
 
 ser = serial.Serial('/dev/cu.usbmodem14101', 9600)
 time.sleep(2)
@@ -39,89 +36,59 @@ def getReading():
     return data, temperature, humidity
 
 def draw_plot(data):
-    fig = Figure()
-    ax = fig.add_subplot(111)
-    ax.set_xlabel("Temperature axis")
-    ax.set_ylabel("Time axis")
-    ax.grid()
-
-    print("data:", data)
     labels = 'Moisuture Percentage', 'Dry Percentage'
     plt.pie(data, labels=labels, colors=['blue', 'brown'], autopct='%1.1f%%', shadow=True, startangle=90)
     plt.axis('equal')
-    #plt.show(block=False)
-    fig = plt.gcf()
-    return fig
-
-
-def draw_figure(canvas, figure):
-    figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
-    figure_canvas_agg.draw()
-    figure_canvas_agg.get_tk_widget().pack(side='top', fill='both', expand=1)
-
-    return figure_canvas_agg
+    plt.show(block=False)
 
 
 data, temperature, humidity = getReading()
-
-layout = [[sg.Cancel(), sg.Button('Update')],
-          [sg.Canvas(size=(640, 480), key='canvas')],
+moisture = data[0]
+layout = [[sg.Button('Show Visual'), sg.Cancel()],
+          [sg.Text('Soil Moisture Percentage: ' + str(data[0]) + '%')],
           [sg.Text('Temperature: ' + str(temperature[0]) + '\u00b0 C')],
           [sg.Text('Humidity: ' + str(humidity) + '%')]]
-window = sg.Window('MagicPot Dashboard', layout, finalize=True)
 
-fig = draw_plot(data)
-canvas_elem = window['canvas']
-graph = FigureCanvasTkAgg(fig, master=canvas_elem.TKCanvas)
-canvas = canvas_elem.TKCanvas
-
-graph.draw()
-figure_x, figure_y, figure_w, figure_h = fig.bbox.bounds
-figure_w, figure_h = int(figure_w), int(figure_h)
-photo = Tk.PhotoImage(master=canvas, width=figure_w, height=figure_h)
-
-canvas.create_image(640 / 2, 480 / 2, image=photo)
-
-figure_canvas_agg = FigureCanvasAgg(fig)
-figure_canvas_agg.draw()
-
-tkagg.blit(photo, figure_canvas_agg.get_renderer()._renderer, colormode=2)
-
+window = sg.Window('MagicPot Dashboard', layout)
 
 while True:
     data, temperature, humidity = getReading()
+    if data[0] < 50:
+        message = "Time to water your plant!"
+        name = "Lukas Griffin"
+        preparedM = 'osascript -e \'tell application \"Messages\" to send \"' + message + '\" to buddy \"' + name + '\"\''
+        os.system(preparedM)
+    if float(temperature[0]) < 10:
+        message = "Your plant is to cold!"
+        name = "Lukas Griffin"
+        preparedM = 'osascript -e \'tell application \"Messages\" to send \"' + message + '\" to buddy \"' + name + '\"\''
+        os.system(preparedM)
     event, values = window.read()
+
     if event in (None, 'Cancel'):
         break
-    elif event == 'Update':
-        window.close()
+    elif event == 'Show Visual':
+        plt.close()
         data, temperature, humidity = getReading()
 
-        layout = [[sg.Cancel(), sg.Button('Update')],
-                  [sg.Canvas(size=(640, 480), key='canvas')],
+        if data[0] < 50:
+            message = "Time to water your plant!"
+            name = "Lukas Griffin"
+            preparedM = 'osascript -e \'tell application \"Messages\" to send \"' +message + '\" to buddy \"' + name + '\"\''
+            os.system(preparedM)
+        if float(temperature[0]) < 10:
+            message = "Your plant is to cold!"
+            name = "Lukas Griffin"
+            preparedM = 'osascript -e \'tell application \"Messages\" to send \"' +message + '\" to buddy \"' + name + '\"\''
+            os.system(preparedM)
+
+        draw_plot(data)
+        window.close()
+        layout = [[sg.Button('Show Visual'), sg.Cancel()],
+                  [sg.Text('Soil Moisture Percentage: ' + str(data[0]) + '%')],
                   [sg.Text('Temperature: ' + str(temperature[0]) + '\u00b0 C')],
                   [sg.Text('Humidity: ' + str(humidity) + '%')]]
-        window = sg.Window('MagicPot Dashboard', layout, finalize=True)
 
-        fig = draw_plot(data)
-        canvas_elem1 = window['canvas']
-        graph1 = FigureCanvasTkAgg(fig, master=canvas_elem1.TKCanvas)
-        canvas1 = canvas_elem1.TKCanvas
-
-        graph1.draw()
-        figure_x, figure_y, figure_w, figure_h = fig.bbox.bounds
-        figure_w, figure_h = int(figure_w), int(figure_h)
-        photo1 = Tk.PhotoImage(master=canvas1, width=figure_w, height=figure_h)
-
-        canvas1.create_image(640 / 2, 480 / 2, image=photo1)
-
-        figure_canvas_agg1 = FigureCanvasAgg(fig)
-        figure_canvas_agg1.draw()
-
-        tkagg.blit(photo1, figure_canvas_agg1.get_renderer()._renderer, colormode=2)
-
-
-        #fig = draw_plot(data)
-        #fig_canvas_agg = draw_figure(window['-CANVAS-'].TKCanvas, fig)
+        window = sg.Window('MagicPot Dashboard', layout)
 
 window.close()
